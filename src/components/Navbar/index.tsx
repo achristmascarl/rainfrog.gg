3931964035440ba0650657eb865a1b50338be4ba
achristmascarl <carl.z.liu@gmail.com>
2023-10-29 17:01:29 -0400

    update navbar styling

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,9 @@
+import styles from "./index.module.scss";
+import { c } from "@/utils";
+
 export default function Navbar({ className }: { className: string }) {
   return (
-    <nav className={className}>
+    <nav className={c(className, styles.Navbar)}>
       <p>yo</p>
     </nav>
   );
