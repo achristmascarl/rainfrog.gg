beee60378fc09d639a25ed513e6d15530901a879
achristmascarl <carl.z.liu@gmail.com>
2023-11-26 15:05:15 -0500

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import type { Metadata } from "next";
-import Head from "next/head";
 import { AppProps } from "next/app";
 import { usePathname } from "next/navigation";
 import { Ubuntu_Mono, Cutive_Mono } from "next/font/google";
@@ -20,11 +18,6 @@ const cutiveMono = Cutive_Mono({
   variable: "--font-cutive-mono",
 });
 
-export const metadata: Metadata = {
-  title: "rainfrog.gg",
-  description: "tattoos",
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const pathname = usePathname();
@@ -35,18 +28,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head>
-        <link
-          rel="icon"
-          href={
-            "data:image/svg+xml," +
-            "<svg xmlns=%22http://www.w3.org/2000/svg%22 " +
-            "viewBox=%220 0 100 100%22>" +
-            "<text y=%22.9em%22 font-size=%2290%22>🐸</text>" +
-            "</svg>"
-          }
-        />
-      </Head>
       <Navbar
         className={c(ubuntuMono.variable, cutiveMono.variable)}
         mobileMenuOpen={mobileMenuOpen}
